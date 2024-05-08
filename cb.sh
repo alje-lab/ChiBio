902b876c54060c42bd93dff4b97208d49f443ead
Christopher Brown <pyrrhicpachyderm@gmail.com>
2024-05-08 13:44:02 +1000

    Have cb.sh also automatically determine IP address in the commented-out line.

--- cb.sh
+++ cb.sh
@@ -1,4 +1,4 @@
 # Uncomment the following line to run ChiBio in the background
-# screen -dmS ChiBio bash -c "gunicorn -b 192.168.7.2:5000 app:application"
+# screen -dmS ChiBio bash -c "gunicorn -b "$(hostname -I | cut -d ' ' -f1):5000" app:application"
 # Then, comment out the next line
 gunicorn -b "$(hostname -I | cut -d ' ' -f1):5000" app:application
